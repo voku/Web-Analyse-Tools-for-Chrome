c01fc58147c04cef44250aacf1362f3d978ce680
Lars Moelleken <voku@voku-online.de>
2016-01-11 21:39:16 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -55,24 +55,28 @@ document.addEventListener('DOMContentLoaded', function() {
     var html = ' \
     <h3>' + url + '</h3> \
     <ul> \
-      <li><a href="https://securityheaders.io/?q=' + urlEncoded + '" target="_blank">Check Security-Header</a></li> \
-      <li><a href="http://www.webpagetest.org/" target="_blank">WebPagetest: Speed-Test</a></li> \
-      <li><a href="http://tools.pingdom.com/fpt/#!/' + urlEncoded + '" target="_blank">Pingdom: Speed-Test</a></li> \
-      <li><a href="http://developers.google.com/speed/pagespeed/insights/?url=' + urlEncoded + '" target="_blank">Google: Speed-Test</a></li> \
+      <li><a href="http://tenon.io/testNow.php?url=' + urlEncoded + '" target="_blank">Check Accessibility (Tenon.io)</a></li>\
+      <li><a href="http://www.webpagetest.org/" target="_blank">Check Performance (WebPageTest)</a></li> \
+      <li><a href="http://tools.pingdom.com/fpt/#!/' + urlEncoded + '" target="_blank">Check Performance (Pingdom)</a></li> \
+      <li><a href="http://developers.google.com/speed/pagespeed/insights/?url=' + urlEncoded + '" target="_blank">Check Performance (Google)</a></li> \
+      <li><a href="https://securityheaders.io/?q=' + urlEncoded + '" target="_blank">Check Security-Headers</a></li> \
       <li><a href="http://cssstats.com/stats?url=' + urlEncoded + '" target="_blank">Show CSS-Statistics</a></li> \
-      <li><a href="http://builtwith.com/?' + urlEncoded + '" target="_blank">Recognize Website-Software</a></li> \
+      <li><a href="http://builtwith.com/?' + urlEncoded + '" target="_blank">Show Website-Software</a></li> \
       <li><a href="http://moz.com/researchtools/ose/links?site=' + urlEncoded + '" target="_blank">Check Inbound Links (OSE)</a></li> \
       <li><a href="http://www.zippy.co.uk/keyworddensity/index.php?url=' + urlEncoded + '&amp;keyword=" target="_blank">Check Keyword-Density</a></li> \
       <li><a href="http://webcache.googleusercontent.com/search?strip=1&amp;q=cache:' + urlEncoded + '" target="_blank">Check Google-Cache</a></li> \
-      <li><a href="http://quixapp.com/headers/?r=' + urlEncoded + '" target="_blank">Check Header-Info</a></li> \
+      <li><a href="http://quixapp.com/headers/?r=' + urlEncoded + '" target="_blank">Check HTTP-Header</a></li> \
       <li><a href="http://linter.structured-data.org/?url=' + urlEncoded + '" target="_blank">Structured Data Linter</a></li> \
-      <li><a href="http://www.google.com/webmasters/tools/richsnippets?q=' + urlEncoded + '" target="_blank">Cbeck Rich Snippets</a></li> \
+      <li><a href="http://www.google.com/webmasters/tools/richsnippets?q=' + urlEncoded + '" target="_blank">Check Rich Snippets</a></li> \
       <li><a href="http://developers.facebook.com/tools/debug/og/object?q=' + urlEncoded + '" target="_blank">Facebook Debugger</a></li> \
       <li><a href="https://developers.pinterest.com/tools/url-debugger/?link=' + urlEncoded + '" target="_blank">Pinterest Rich Pins Validator</a></li> \
+      <li><a href="http://richpreview.com/?url=' + urlEncoded + '" target="_blank">Rich Preview</a></li> \
       <li><a href="http://validator.w3.org/check?uri=' + urlEncoded + '" target="_blank">HTML-Validator</a></li> \
       <li><a href="http://jigsaw.w3.org/css-validator/validator?uri=' + urlEncoded + '" target="_blank">CSS-Validator</a></li> \
       <li><a href="https://dev.modern.ie/tools/staticscan/?url=' + urlEncoded + '" target="_blank">Modern IE Site Scan</a></li> \
-      <li><a href="https://www.google.com/webmasters/tools/mobile-friendly/?url=' + urlEncoded + '" target="_blank">Mobile-Friendly-Test</a></li> \
+      <li><a href="https://www.google.com/webmasters/tools/mobile-friendly/?url=' + urlEncoded + '" target="_blank">Check Mobile-Friendly</a></li> \
+      <li><a href="https://www.ssllabs.com/ssltest/analyze.html?d=' + window.location.host + '" target="_blank">Check SSL</a></li> \
+      <li><a href="https://www.mail-tester.com/" target="_blank">Check Mail-Server</a></li> \
     </ul>';
 
     document.getElementById('js-output').innerHTML = html;
